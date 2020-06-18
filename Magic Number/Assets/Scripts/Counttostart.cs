eda9f8f9e60dc9f3c1e6eb57e4acc1e80bab0e5c
Heath McMillen <hgmcmillen@gmail.com>
2020-06-18 08:29:25 -0600

    Added the count in screen

---
+++ Magic Number/Assets/Scripts/Counttostart.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Counttostart : MonoBehaviour
+{
+    public int countInTime;
+    public Text countInDisplay;
+
+    public string sceneAfter;
+
+    IEnumerator countIn()
+    {
+        while (countInTime > 0)
+        {
+            countInDisplay.text = countInTime.ToString();
+            yield return new WaitForSeconds(1f);
+            countInTime--;
+        }
+        countInDisplay.text = "Go!";
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneAfter);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        countInTime = 3;
+        StartCoroutine(countIn());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
